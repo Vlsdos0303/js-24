56c7ee62a5bcc2c4b603552113a257e9b1507890
Admin <admin@Admins-MacBook-Pro.local>
2023-10-26 13:07:44 +0300

    upd

--- 1.js
+++ 1.js
@@ -10,14 +10,32 @@ function task1() {
    */
   function getData(data, timeout = 2000) {
     // Функція повертає новий проміс
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        if(Object.keys(data).length) {
+          resolve(data);
+        } else {
+          reject(new Error("Об'єкт пустий"))
+        }
+      }, timeout);
+    }); 
     // За допомогою setTimeout ми симулюємо затримку timeout, яка виникає при роботі з асинхронними джерелами даних
     // Якщо об'єкт не пустий, ми викликаємо resolve з data
     // Якщо об'єкт пустий, ми викликаємо reject з новим об'єктом Error("Об'єкт пустий")
   }
 
   // Ми викликаємо getData з об'єктом { name: "John", age: 30, city: "New York" } і часом очікування 2000
+  let promise = getData({ name: "John", age: 30, city: "New York" }, 2000)
 
   // Ми обробляємо дані, повернуті промісом
+  promise
+  .then(
+    (data) => console.log(data),
+    (error) => console.log(error)
+  )
+  .finally(
+    () => console.log("Завдання 1 завершено")
+  )
   // У разі успіху виводимо в консоль дані
   // У разі помилки виводимо повідомлення помилки
   // Незалежно від того, завершилось виконання проміса успіхом чи ні виводимо в консоль "Завдання 1 завершено"
